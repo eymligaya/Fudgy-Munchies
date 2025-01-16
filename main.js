62a547b260be6c68458a0cd18c97d48144fc2333
meeeeeeeeeeeeeeeejjjjjjjjjjjjjjjjjjjjjjjjj <mariariyaegalin@gmail.com>
2025-01-17 00:33:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,12 @@ function applyTheme() {
     }
 }
 
+// for chatbot //
 
+const secret = '•••••••••'; // Your verification secret key
+const userId = current_user.id // A string UUID to identify your user
+
+const hash = crypto.createHmac('sha256', secret).update(userId).digest('hex');
 
 
 
